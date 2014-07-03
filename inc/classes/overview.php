9c5b92a0d50094902304c8f41e87f96b0746f4eb
digital-ghost <digital.ghost@network.stalker.fi>
2014-07-03 11:15:48 +0400

    Better overview

--- inc/classes/overview.php
+++ inc/classes/overview.php
@@ -10,25 +10,31 @@ class Overview extends Scanlab {
     }
 
     function GET($matches) {
+        $time_start = microtime(true);
+
         if (USE_WORKER === true) {
             $distinct = $this->db->cache->findOne(array("key" => "distinct_cache"));
             if ($distinct == NULL) {
                 if ($this->user === ROOT_USER) {
-                    showError("No cached version available. You can manually update cache at <a href='".REL_URL
-                        ."root/stats'>root panel</a>.");
+                    $this->renderError("No cached version available. You can manually update cache at root panel.");
                 }
-                showError("No cached version available.");
+                $this->renderError("No cached version available.");
             }
             $distinct = unserialize($distinct["value"]);   
         } else {
             $distinct = getDistinct($this->db);
         }
 
-        $vars = $distinct;
-        $vars["total_count"] = $this->db->reports->count();
-        
-        $this->view('overview.html', $vars);
+        function cmp($a, $b){
+            return $b['count'] - $a['count'];
+        }
 
+        usort($distinct['ports'], 'cmp');
+        usort($distinct['services'], 'cmp');
+        usort($distinct['countries'], 'cmp');
+        usort($distinct['tags'], 'cmp');
+        $distinct["total_count"] = $this->db->reports->count();
+        $this->view('overview.html', $distinct);
     }
 
 }
